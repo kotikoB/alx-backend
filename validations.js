21fbaef493ef8b0bf28f154890ae682bd08f8e90
kotikoB <otieno.steve704@gmail.com>
2020-10-10 13:17:43 +0300

    add authentication and mongoose models

---
+++ validations.js
@@ -0,0 +1,22 @@
+const Joi = require('@hapi/joi');
+
+// register validation
+const registerValidation = (data) => {
+    const validationSchema = Joi.object().keys({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return validationSchema.validate(data);
+};
+
+// login validation
+const loginValidation = (data) => {
+    const validationSchema = Joi.object().keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return validationSchema.validate(data);
+};
+
+module.exports = { registerValidation, loginValidation };
